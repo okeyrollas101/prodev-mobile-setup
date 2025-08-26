68e31f4bfc430c46d6ba8a90c13e8fe42fcb07fa
Mac <mac@okey101.local>
2025-08-26 18:15:52 +0100

    Modified app/index,tsx

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Mobile App all setup!!</Text>
     </View>
   );
 }
